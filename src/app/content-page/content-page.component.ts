e4402d6d89f1d8929c4a0f91890e6eeb5368be01
DavidMcMorrow <mcmorrda@tcd.ie>
2021-07-08 11:11:41 +0100

    able to seperate the posts into user and other user

--- src/app/content-page/content-page.component.ts
+++ src/app/content-page/content-page.component.ts
@@ -12,12 +12,27 @@ export class ContentPageComponent implements OnInit {
 
   userPosts : any = []
   otherPosts : any = []
+  allPosts : any = []
 
   ngOnInit(): void {
     console.log("this.appService.usersInfo", this.appService.userInfo);
+    this.allPosts = this.appService.allPosts
     console.log("this.appService.allPosts", this.appService.allPosts);
-    this.appService.allPosts
+    this.seperatingPosts(this.appService.userInfo.id)
+    console.log("this.userPost", this.userPosts);
+    console.log("this.otherPosts", this.otherPosts)
   }
 
+  seperatingPosts(userID : any){
+    for (let index = 0; index < this.allPosts.length; index++) {
+      const element = this.allPosts[index];
+      if (element.userId == userID) {
+        this.userPosts.push(element)
+      }
+      else{
+        this.otherPosts.push(element)
+      }
+    }
+  }
   
 }
